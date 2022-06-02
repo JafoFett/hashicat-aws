3da4933c6164b0094555a962cfa334e38f4c1530
JafoFett <jeff.ronaghan@nov.com>
2022-06-02 16:18:16 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,8 +1,8 @@
 module "s3-bucket" {
   source  = "app.terraform.io/terraform-demo-jronaghan/s3-bucket/aws"
   version = "2.8.0"
-  bucket_prefix = ${var.prefix}
-  bucket = "my-s3-bucket23423423"
+  
+  bucket = "${var.prefix}-my-s3-bucket23423423"
   acl    = "private"
 
   versioning = {
